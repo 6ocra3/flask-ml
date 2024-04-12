de72e84ce06ada94fe3690b571331055d13ead4e
6ocra3 <6o3kmm@mail.ru>
2024-04-12 09:19:37 +0300

    add mock reports

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from starlette.responses import FileResponse
+import os
 import tflr
 app = FastAPI()
 tflr.init()
@@ -17,3 +19,12 @@ async def process_text(item: TextItem):
     print(tflr.predict(item.text))
     prediction = tflr.predict(item.text)
     return prediction
+
+@app.get("/get_reports/")
+async def download_file():
+    file_path = f"./reports.csv"
+    # Проверяем, существует ли файл
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    # Возвращаем файл как ответ
+    return FileResponse(file_path, filename="reports.csv")
